7a98c890122408d8c9bd03c9e4fdd0647b27ab5c
Mihael Isaev <isaev.mihael@gmail.com>
2019-03-12 21:31:23 +0400

    Initial Commit

---
+++ SwifRootViewController/SwifRootViewController.h
@@ -0,0 +1,19 @@
+//
+//  SwifRootViewController.h
+//  SwifRootViewController
+//
+//  Created by Mihael Isaev on 12/03/2019.
+//  Copyright © 2019 Mihael Isaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwifRootViewController.
+FOUNDATION_EXPORT double SwifRootViewControllerVersionNumber;
+
+//! Project version string for SwifRootViewController.
+FOUNDATION_EXPORT const unsigned char SwifRootViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwifRootViewController/PublicHeader.h>
+
+
---
+++ SwifRootViewControllerTests/SwifRootViewControllerTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwifRootViewControllerTests.swift
+//  SwifRootViewControllerTests
+//
+//  Created by Mihael Isaev on 12/03/2019.
+//  Copyright © 2019 Mihael Isaev. All rights reserved.
+//
+
+import XCTest
+@testable import SwifRootViewController
+
+class SwifRootViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
